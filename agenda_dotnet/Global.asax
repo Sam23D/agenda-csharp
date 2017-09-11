0d3c29292ea3f731304067b98a783c2f322f7157
Samuel Blanco <dev.sam23d@gmail.com>
2017-09-11 04:15:32 -0600

    All working
    Done with VS2015

---
+++ agenda_dotnet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="agenda_dotnet.MvcApplication" Language="C#" %>
---
+++ agenda_dotnet/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace agenda_dotnet.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [DisplayName( "Phone Type" )]
+        public PhoneType PhoneType { get; set; }
+    }
+
+    public enum PhoneType{
+        Home,
+        Work, 
+        Other
+    }
+}
